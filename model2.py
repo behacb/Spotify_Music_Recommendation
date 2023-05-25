721f4efa582445ca2845951b484e659fd4ac3859
Bedirhan Hasançebi <102032422+behacb@users.noreply.github.com>
2023-05-25 11:38:51 +0300

    Update model2.py

--- model2.py
+++ model2.py
@@ -1,4 +1,5 @@
 import spotipy as sp
+import pandas as pd
 from sklearn.metrics.pairwise import manhattan_distances
 from spotipy.oauth2 import SpotifyClientCredentials
 cid = "eb018f76e1bb427caf2857fccc6cc719"
@@ -8,10 +9,20 @@ sp = sp.Spotify(client_credentials_manager=client_credentials_manager)
 def model_2(df,song_1):
     df['sim'] = manhattan_distances(df.drop(['id', 'id_artists'], axis=1), song_1.drop(['id', 'id_artists'], axis=1))
     df_model = df.sort_values('sim', ascending=True)
-    qq = df_model.groupby('id_artists').head(5).id.head(50)  # to limit recmmendation by same artist
-    aa = sp.tracks(qq[0:50])
-    Fresult_2=[]
-    for i in range(50):
-        Fresult_2.append(aa['tracks'][i]['name'])
+    qq = df_model.groupby('id_artists').head(5).id.head(10)  # to limit recmmendation by same artist
+    aa = sp.tracks(qq[0:10])
+    Fresult_2_name = []
+    Fresult_2_artistname=[]
+    Fresult_2_preview_url=[]
+    Fresult_2_image=[]
+    for i in range(10):
+        Fresult_2_name.append(aa['tracks'][i]['name'])
+        Fresult_2_artistname.append(aa["tracks"][i]["album"]["artists"][0]["name"])
+        Fresult_2_preview_url.append(aa["tracks"][i]["preview_url"])
+        try:
+            Fresult_2_image.append(aa["tracks"][i]["album"]["images"][0]["url"])
+        except:
+            None
+
     df.drop("sim", axis=1, inplace=True)
-    return Fresult_2
+    return Fresult_2_name, Fresult_2_artistname, Fresult_2_preview_url, Fresult_2_image
